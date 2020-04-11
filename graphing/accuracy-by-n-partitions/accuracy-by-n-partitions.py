5c3953f6cc893e7d8939dff0a694b14d57c56daa
rdimaio <riccardodimaio11@gmail.com>
2020-04-11 19:56:16 +0800

    Add accuracy-by-n-partitions

---
+++ graphing/accuracy-by-n-partitions/accuracy-by-n-partitions.py
@@ -0,0 +1,184 @@
+import subprocess
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import itertools
+
+import os
+from scipy import stats
+# local dependencies
+from aptbps import aptbps
+from modelnet40 import load_modelnet40
+
+import csv
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+PROJECT_DIR = os.path.dirname(SCRIPT_DIR)
+DATA_PATH = os.path.join(PROJECT_DIR, 'data')
+ACCS_PATH = os.path.join(DATA_PATH, 'accs')
+LOGS_PATH = os.path.join(PROJECT_DIR, 'logs')
+
+N_MODELNET_CLASSES = 40
+
+N_BPS_POINTS = 512
+BPS_RADIUS = 1
+
+n_parts = 5
+
+# Each .csv file has 1200 rows representing the accuracy at each epoch.
+# There are 20 columns, each one representing the number of partitions
+# used to encode the input dataset.
+
+triangle_fftkde_name = 'triangle-fftkde_mlp.csv'
+
+with open(os.path.join(ACCS_PATH, triangle_fftkde_name)) as fd:
+    reader = csv.reader(fd)
+    # 1191 -> 1201 because of the header
+    last10 = [row for idx, row in enumerate(reader) if idx in np.arange(1191,1201)]
+    last10 = np.asarray(last10, dtype=np.float64)
+
+    triangle_fftkde_accs = np.zeros([last10.shape[1]])
+    triangle_fftkde_sems = np.zeros([last10.shape[1]])
+
+    for i in range(0, last10.shape[1]): # Iterate over columns (n. of partitions)
+
+        # Get the accuracies of last 10 epochs for that column (n. of partitions)
+        last10_curr_col = last10[0:, i]
+
+        triangle_fftkde_accs[i] = np.mean(last10_curr_col)
+        triangle_fftkde_sems[i] = stats.sem(last10_curr_col)
+
+comp_fftkde_name = 'comp-fftkde_mlp.csv'
+
+with open(os.path.join(ACCS_PATH, comp_fftkde_name)) as fd:
+    reader = csv.reader(fd)
+    # 1191 -> 1201 because of the header
+    last10 = [row for idx, row in enumerate(reader) if idx in np.arange(1191,1201)]
+    last10 = np.asarray(last10, dtype=np.float64)
+
+    comp_fftkde_accs = np.zeros([last10.shape[1]])
+    comp_fftkde_sems = np.zeros([last10.shape[1]])
+
+    for i in range(0, last10.shape[1]): # Iterate over columns (n. of partitions)
+
+        # Get the accuracies of last 10 epochs for that column (n. of partitions)
+        last10_curr_col = last10[0:, i]
+
+        comp_fftkde_accs[i] = np.mean(last10_curr_col)
+        comp_fftkde_sems[i] = stats.sem(last10_curr_col)
+    
+comp_gkde_name = 'comp-gkde_mlp.csv'
+
+with open(os.path.join(ACCS_PATH, comp_gkde_name)) as fd:
+    reader = csv.reader(fd)
+    # 1191 -> 1201 because of the header
+    last10 = [row for idx, row in enumerate(reader) if idx in np.arange(1191,1201)]
+    last10 = np.asarray(last10, dtype=np.float64)
+
+    comp_gkde_accs = np.zeros([last10.shape[1]])
+    comp_gkde_sems = np.zeros([last10.shape[1]])
+
+    for i in range(0, last10.shape[1]): # Iterate over columns (n. of partitions)
+
+        # Get the accuracies of last 10 epochs for that column (n. of partitions)
+        last10_curr_col = last10[0:, i]
+
+        comp_gkde_accs[i] = np.mean(last10_curr_col)
+        comp_gkde_sems[i] = stats.sem(last10_curr_col)
+
+triangle_gkde_name = 'triangle-gkde_mlp.csv'
+
+with open(os.path.join(ACCS_PATH, triangle_gkde_name)) as fd:
+    reader = csv.reader(fd)
+    # 1191 -> 1201 because of the header
+    last10 = [row for idx, row in enumerate(reader) if idx in np.arange(1191,1201)]
+    last10 = np.asarray(last10, dtype=np.float64)
+
+    triangle_gkde_accs = np.zeros([last10.shape[1]])
+    triangle_gkde_sems = np.zeros([last10.shape[1]])
+
+    for i in range(0, last10.shape[1]): # Iterate over columns (n. of partitions)
+
+        # Get the accuracies of last 10 epochs for that column (n. of partitions)
+        last10_curr_col = last10[0:, i]
+
+        triangle_gkde_accs[i] = np.mean(last10_curr_col)
+        triangle_gkde_sems[i] = stats.sem(last10_curr_col)
+
+bps_name = 'bps_mlp.csv'
+
+with open(os.path.join(ACCS_PATH, bps_name)) as fd:
+    reader = csv.reader(fd)
+    # 1191 -> 1201 because of the header
+    last10 = [row for idx, row in enumerate(reader) if idx in np.arange(1191,1201)]
+    last10 = np.asarray(last10, dtype=np.float64)
+
+    bps_acc = np.mean(last10)
+    bps_sem = stats.sem(last10)
+
+fig, ax = plt.subplots()
+ax.set_title("Accuracy by number of partitions", weight='bold')
+
+ax.yaxis.grid(True, color='#EEEEEE')
+fig.tight_layout()
+ax.set_ylabel("Accuracy (%)")
+ax.set_xlabel("Number of partitions")
+#ax.set_yscale("log")
+
+marker = itertools.cycle((',', '^', 's', 'D', 'o')) 
+linestyle = itertools.cycle(("-","--","-.",(0, (3, 1, 1, 1))))
+
+x = np.arange(0, 19)
+
+plt.axhline(y=bps_acc, color='b', linestyle='dotted', label="bps MLP", marker=next(marker))
+plt.errorbar(0, bps_acc, yerr=bps_sem,capsize=5)
+#plt.plot(accs, color='g', label="APTBPS-MLP", marker="o")
+
+#plt.plot(triangle_gkde_accs, color='C1', linestyle=next(linestyle), label="triangle-gkde MLP")
+plt.errorbar(x, triangle_gkde_accs, linestyle=next(linestyle), yerr=triangle_gkde_sems, marker='o', markersize=4, capsize=5, label="triangle-gkde MLP")
+
+plt.errorbar(x, comp_gkde_accs, linestyle=next(linestyle), marker='o', yerr=comp_gkde_sems, markersize=4, capsize=5, label="comp-gkde MLP")
+#plt.plot(comp_gkde_accs, linestyle='--', label="comp-gkde MLP")
+
+#plt.plot(triangle_fftkde_accs,linestyle=next(linestyle), label="triangle-fftkde MLP")
+plt.errorbar(x, triangle_fftkde_accs,yerr=triangle_fftkde_sems, linestyle=next(linestyle), marker='o', markersize=4, capsize=5, label="triangle-fftkde MLP")
+
+#plt.plot(comp_fftkde_accs, linestyle=next(linestyle), label="comp-fftkde MLP")
+plt.errorbar(x, comp_fftkde_accs, yerr=comp_fftkde_sems, linestyle=next(linestyle), marker='o', markersize=4, capsize=5, label="comp-fftkde MLP")
+
+#plt.xlim(left=1, right=20)
+#plt.yticks(ticks=accs)
+plt.xticks(np.arange(len(triangle_fftkde_accs)), np.arange(2, len(triangle_fftkde_accs)+2))
+#plt.xticks(np.arange(len(accs)), np.arange(1, len(accs)+1))
+plt.grid(linestyle='--')
+plt.legend(loc="upper right")
+plt.show()
+
+print("values: ")
+
+print("bps mean acc:")
+print(bps_acc)
+print("bps sem:")
+print(bps_sem)
+
+print("triangle gkde mean accs:")
+print(triangle_gkde_accs)
+print("triangle gkde sems:")
+print(triangle_gkde_sems)
+
+print("comp gkde mean accs:")
+print(comp_gkde_accs)
+print("comp gkde sems:")
+print(comp_gkde_sems)
+
+print("triangle fftkde mean accs:")
+print(triangle_fftkde_accs)
+print("triangle fftkde sems:")
+print(triangle_fftkde_sems)
+
+print("comp fftkde mean accs:")
+print(comp_fftkde_accs)
+print("comp fftkde sems:")
+print(comp_fftkde_sems)
